e1922e98f779a6fe51e551a0476a48c6ba4acbd7
xandoalka <xandoalka@gmail.com>
2024-07-09 07:27:10 +0700

    update model

--- prisma/migrations/20240709002644_app/migration.sql
+++ prisma/migrations/20240709002644_app/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Item` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "Item";
-
 -- CreateTable
 CREATE TABLE "App" (
     "id" SERIAL NOT NULL,

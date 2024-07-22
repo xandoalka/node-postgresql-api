43ac924340d8513e1d63745963290c450b6e5fab
xandoalka <xandoalka@gmail.com>
2024-07-22 09:13:45 +0700

    update dataProject

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(cors({ origin: "*" })); // Use cors middleware
 const appRouter = require('./routes/app.routes');
 const dataProjectRouter = require('./routes/dataProject.routes');
 app.use("/", appRouter);
-app.use("/project", dataProjectRouter);
+app.use("/project/", dataProjectRouter);
 
 app.listen(process.env.PORT, () => console.log("Server is running on port 5000"));
